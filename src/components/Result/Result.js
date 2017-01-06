84141120a794c5da9637f2c66f710137466438af
Bersam Karbasion <bersam.k@gmail.com>
2017-01-06 14:13:58 +0330

    add result component

---
+++ src/components/Result/Result.js
@@ -0,0 +1,53 @@
+/**  -*- mode: react; -*-
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React, { PropTypes } from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './Result.css';
+
+class Header extends React.Component {
+  static propTypes = {
+    result: PropTypes.object.isRequired,
+  };
+
+  render() {
+    return (
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th>
+              نشانه
+            </th>
+            <th>
+              نمره خام
+            </th>
+            <th>
+              نمره نهایی BR
+            </th>
+            <th>
+              مقیاس‌
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(this.props.result).map(key => (
+            <tr className={s.newsItem}>
+              <td>{key}</td>
+              <td>{this.props.result[key]}</td>
+              <td>{this.props.result[key]}</td>
+              <td />
+            </tr>
+           ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default withStyles(s)(Header);

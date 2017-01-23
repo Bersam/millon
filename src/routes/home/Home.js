9be0d65fb028797d37b05089c244b749a3b0f56c
Bersam Karbasion <bersam.k@gmail.com>
2017-01-23 12:47:19 +0330

    add gender

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -8,7 +8,7 @@
  */
 
 import React, { PropTypes } from 'react';
-import { Pagination, Row, Col } from 'react-bootstrap';
+import { Pagination, Row, Col, ButtonGroup, Button } from 'react-bootstrap';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.css';
 import Result from '../../components/Result';
@@ -41,6 +41,12 @@ class Home extends React.Component {
     });
   };
 
+  handleGenderChange = (status) => {
+    this.setState({
+      gender: status,
+    });
+  };
+
   handleOptionChange = (index, status) => {
     if (this.state.questions[index].status === status) {
       return;
@@ -68,11 +74,36 @@ class Home extends React.Component {
     const toNo = this.state.activePage * 10;
     const questions = this.state.questions.slice(fromNo, toNo);
     const maxPage = parseInt((this.state.questions.length / 10) + 1, 0);
+    const gender = this.state.gender;
     return (
       <div className={s.root}>
         <div className={s.container}>
           <h1>سوال‌ها</h1>
-          <p>به همه‌ی سوالات پاسخ دهید.</p>
+          <Row>
+            <Col md={4}>
+              <p>به همه‌ی سوالات پاسخ دهید.</p>
+            </Col>
+            <Col md={4} className="text-left">
+              <p>جنسیت خود را مشخص کنید</p>
+            </Col>
+            <Col md={4} className="text-right">
+              <ButtonGroup>
+                <Button
+                  className={gender === 'male' && 'btn-primary'}
+                  onClick={() => this.handleGenderChange('male')}
+                >
+                  مرد
+                </Button>
+                <Button
+                  className={gender === 'female' && 'btn-primary'}
+                  onClick={() => this.handleGenderChange('female')}
+                >
+                  زن
+                </Button>
+              </ButtonGroup>
+            </Col>
+          </Row>
+          <hr />
           <Row>
             <Col className="text-center">
               <Pagination
@@ -95,7 +126,7 @@ class Home extends React.Component {
            ))}
           <hr />
           {(this.state.activePage === maxPage) &&
-          <Result result={this.state.result} />
+          <Result result={this.state.result} gender={this.state.gender} />
           }
           <Row>
             <Col className="text-center">

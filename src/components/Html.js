0fc9ca04caab17b60becb259d74ba4c29a5fb828
Bersam Karbasion <bersam.k@gmail.com>
2017-01-30 01:10:07 +0330

    add keywords

--- src/components/Html.js
+++ src/components/Html.js
@@ -28,6 +28,7 @@ class Html extends React.Component {
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <title>{title}</title>
           <meta name="description" content={description} />
+          <meta name="keywords" content="تست میلون, پرسشنامه رایگان میلون, MCMI فارسی, تست روانشناسی, تشخیص اختلال بزرگسالان" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="apple-touch-icon" href="apple-touch-icon.png" />
           {style && <style id="css" dangerouslySetInnerHTML={{ __html: style }} />}
